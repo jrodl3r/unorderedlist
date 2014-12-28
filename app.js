ed6844fd39647ffa24406df60fe678517135482d
John Rodler <john@jrod.io>
2014-12-28 18:52:12 -0500

    setup basic socket.io add pastey emitter

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
+
 var express = require('express'),
-    app = express(),
-    server = require('http').createServer(app),
-    io = require('socket.io').listen(server);
+    app     = express(),
+    server  = require('http').createServer(app),
+    io      = require('socket.io').listen(server);
 
 server.listen(3000);
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
-
 app.use('/css', express.static(__dirname + '/css'));
 app.use('/js', express.static(__dirname + '/js'));
+
+io.sockets.on('connection', function(socket){
+  socket.on('add pastey', function(data){
+    io.sockets.emit('add pastey', data);
+  });
+});
--- js/core.js
+++ js/core.js
@@ -1,1 +1,19 @@
-console.log('winning!');
+
+$(document).ready(function() {
+
+  var socket       = io.connect(),
+      pastey_form  = $('#pastey-form'),
+      pastey_input = $('#new-pastey'),
+      pastey_list  = $('#pastey-list');
+
+  pastey_form.submit(function(e) {
+    e.preventDefault();
+    socket.emit('add pastey', pastey_input.val());
+    pastey_input.val('');
+  });
+
+  socket.on('add pastey', function(data) {
+    pastey_list.append('<li>' + data + '</li>');
+  });
+
+});
