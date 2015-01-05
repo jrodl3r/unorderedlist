324b68534bb1ee8b8f5d35cc6061e0cbf3dab007
John Rodler <john@jrod.io>
2015-01-04 22:15:38 -0500

    add empty ip to server.listen (fix for nitrous.io)

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 // init server
 var express  = require('express'),
     app      = express(),
-    server   = require('http').createServer(app),
+    server   = require('http').createServer(app).listen(3000, '0.0.0.0'),
     io       = require('socket.io').listen(server),
     mongoose = require('mongoose');
-
-server.listen(3000);
 
 
 // init mongodb
